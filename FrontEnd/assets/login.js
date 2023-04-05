2ec54b6ea36de63ab84bf31af6b68fd8ec62e7ef
Yoann Rachad <yoann.pro.rachad@gmail.com>
2023-04-05 05:24:09 +0200

    page login, modal gallery, modal add

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,46 +1,79 @@
+/*
 fetch("http://localhost:5678/api/users/login")
-.then(function(result) {
-	return result.json();
- })
+.then(function(response) {
+	if(response.ok) {
+		return response.json();
+	}
+})
+.then(function(response) {
+	// Le traitement est à écrire ici (et ce commentaire doit s'autodétruire :o)
+})
+.catch(function(err) {
+	console.log(err);
+});
+*/
 
- // On récupère les élements du formulaire
-const form = document.querySelector('myForm');
-const email = document.querySelector('mail');
-const password = document.querySelector('password');
+
+
+// login-form
+// login-form-email
+// login-form-password
+
+
+// On récupère les élements du formulaire
+const form = document.querySelector('#login-form');
+const email = document.querySelector('#login-form-email');
+const password = document.querySelector('#login-form-password');
 
 // On génère des événements et on empêche le compotement par défault
-form.addEventListener('submit', event =>{
-    
-    if(email.value.trim()= ""){
-        let myError = document.getElementById('error-mail');
-        myError.innerText = "Le champs email est requis";
-        myError.style.color="red";
-        event.preventDefault();
-    } 
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
 
+	// On envoie au backend la requête POST avec les champs email / password
+	fetch("http://localhost:5678/api/users/login", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			"email": email.value.trim(),
+			"password": password.value.trim()
+		}),
+	})
+	.then(function(response) {
+		switch(response.status) {
+			case 500:
+			case 503:
+				alert("Erreur serveur");
+			break;
+			case 401:
+			case 404:
+				alert("Email ou password incorrect");
+			break;
+			case 200:
+				console.log("Authentification OK");
+				return response.json();
+			break;
+			default:
+				alert("Erreur inconnue");
+			break;
+		}
+	})
+	.then(function(response) {
+		localStorage.setItem('userId', response.userId);
+		localStorage.setItem('token', response.token);
+	})
+
+    // .then(function(response) {
+	// 	localStorage.removeItem('userId', response.userId);
+	// 	localStorage.removeItem('token', response.token);
+	// })
+
+	.catch(function(err) {
+		console.log(err);
+	});
 })
-   
-// // Fonctions
-// function form_contain(){
-//     // Obtenir toutes les valeurs des inputs
-//     const emailValue = email.value.trim();
-//     const passwordValue = password.value.trim();
 
-//     // On vérifie le champ de l'email
-//     if(emailValue === ""){
-//         let message = "Le champ email est vide";
-//         setError(email, message);
-//     } 
 
-// }
-
-// function setError(elements, message){
-//     const formContain = elements.parentElement;
-//     const errorMail = formContain.querySelector('error_pwd');
-
-//     // Ajout du message d'erreur
-//     error_pwd.innerText = message;
-//     error_pwd.style.color= 'red';
-// }
 
 
